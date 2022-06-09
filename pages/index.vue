46acbfe99508592e15c7ec1e63fb93e4d0b27975
rafaelsene01 <rafaelssene@gmail.com>
2022-06-08 22:54:21 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ ip }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      ip: '',
+    }
+  },
+  mounted() {
+    this.getIp()
+  },
+  methods: {
+    async getIp() {
+      const ip = await this.$axios.$get('http://icanhazip.com')
+      this.ip = ip
+    },
+  },
+}
+</script>
